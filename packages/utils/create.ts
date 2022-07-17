251827808d5222a171a3960e2373c090106cdc54
夜静听雨 <7710500+mao0826@user.noreply.gitee.com>
2022-07-17 09:00:41 +0800

    init

---
+++ packages/utils/create.ts
@@ -0,0 +1,65 @@
+// BEM 规范
+// block 代码块   element 元素  modifier 装饰
+// m-button
+// m-button__element
+// m-button__element--disabled
+// is-checked  is-enabled
+
+const bem = createNameSpace("button");
+console.log(bem.b("box"));
+
+function createNameSpace(name: string) {
+  const prefixName = `m-${name}`;
+  return createBEM(prefixName);
+}
+function createBEM(prefixName: string) {
+  const b = (blockSuffix: string) => _bem(prefixName, blockSuffix);
+  const e = (element?: string) =>
+    element ? _bem(prefixName, "", element) : "";
+  const m = (modifier?: string) =>
+    modifier ? _bem(prefixName, "", "", modifier) : "";
+  const be = (blockSuffix?: string, element?: string) =>
+    blockSuffix && element ? _bem(prefixName, blockSuffix, element) : "";
+  const bm = (blockSuffix?: string, modifier?: string) =>
+    blockSuffix && modifier ? _bem(prefixName, blockSuffix, "", modifier) : "";
+  const em = (element?: string, modifier?: string) =>
+    element && modifier ? _bem(prefixName, "", element, modifier) : "";
+  const bem = (blockSuffix?: string, element?: string, modifier?: string) =>
+    blockSuffix && element && modifier
+      ? _bem(prefixName, blockSuffix, element, modifier)
+      : "";
+  const is = (name: string, state: boolean) => (state ? `is-${name}` : "");
+  return {
+    b,
+    e,
+    m,
+    be,
+    bm,
+    em,
+    bem,
+    is,
+  };
+}
+
+/**
+ * 前缀名字 m-button-box __element--modifier
+ * @param prefixName
+ * @param blockSuffix
+ */
+function _bem(
+  prefixName: string,
+  blockSuffix?: string,
+  element?: string,
+  modifier?: string
+) {
+  if (blockSuffix) {
+    prefixName += `-${blockSuffix}`;
+  }
+  if (element) {
+    prefixName += `__${element}`;
+  }
+  if (modifier) {
+    prefixName += `--${modifier}`;
+  }
+  return prefixName;
+}
